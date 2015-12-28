7bf3580f37ff48ebfdf1215b8fe11c0351d4a25a
hustlei <hustlei@sina.cn>
2015-12-28 21:50:08 +0800

    add initcomponent to app.cs

--- src/EpubViewer/App.xaml.cs
+++ src/EpubViewer/App.xaml.cs
@@ -7,27 +7,35 @@
 
 using System;
 using System.Reflection;
+using System.Windows;
 
 namespace EpubViewer
 {
     /// <summary>
     /// App.xaml 的交互逻辑
     /// </summary>
-    public partial class App
+    public partial class App: Application
     {
+
         public App()
         {
-            //string s = AppDomain.CurrentDomain.BaseDirectory;
-            //Environment.SetEnvironmentVariable("path", s + "occt;" + s + "occt\\3rdparty;" + Environment.GetEnvironmentVariable("path"));
             conf("profiles\\init.conf");
+            try
+            {
+                InitializeComponent();
+            }
+            catch (Exception e)
+            {
+                System.Windows.MessageBox.Show(e.Message);
+            }
         }
 
-        private void conf(string p)
+        private static void conf(string p)
         {
             AppDomain.CurrentDomain.SetData("APP_CONFIG_FILE", p);
             var m = typeof(AppDomainSetup).GetMethod("UpdateContextProperty", BindingFlags.NonPublic | BindingFlags.Static);
             var funsion = typeof(AppDomain).GetMethod("GetFusionContext", BindingFlags.NonPublic | BindingFlags.Instance);
-            m.Invoke(null, new object[] { funsion.Invoke(AppDomain.CurrentDomain, null), "APP_CONFIG_FILE", p });
+            m.Invoke(null, new[] { funsion.Invoke(AppDomain.CurrentDomain, null), "APP_CONFIG_FILE", p });
         }
     }
 }
--- src/EpubViewer/AppBootstrapper.cs
+++ src/EpubViewer/AppBootstrapper.cs
@@ -18,6 +18,7 @@ using System.Globalization;
 using System.IO;
 using System.Diagnostics;
 using System.Linq;
+using System.Windows;
 
 namespace EpubViewer
 {
